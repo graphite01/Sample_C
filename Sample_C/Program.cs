6899f06615ea8767a6533f5e14eccaa06d587ef8
graphite01 <kirito10082279@gmail.com>
2022-12-15 16:23:11 +0900

    最初の記述を参考書通りに修正

--- Sample_C/Program.cs
+++ Sample_C/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // コンソールに文字列を表示する
+            Console.WriteLine("Hello World");
+        }
+    }
+}
