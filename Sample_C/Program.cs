8c5f5d3c8c2e0452413dd5bf76631243777dbfb3
graphite01 <kirito10082279@gmail.com>
2022-12-15 16:33:59 +0900

    コンソールに日本語で名前を表記

--- Sample_C/Program.cs
+++ Sample_C/Program.cs
@@ -7,7 +7,7 @@ namespace Sample
         static void Main(string[] args)
         {
             // コンソールに文字列を表示する
-            Console.WriteLine("Hello, C#");
+            Console.WriteLine("伊藤 凛");
         }
     }
 }
